fa23ecca1effb72d3a981b7969d07b93bc4ba867
RogerioSouza-f <souzarogerio33freitas@gmail.com>
2026-01-18 14:41:51 -0300

    refactor:/ teste unitario AuthControllerTest

---
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/AuthControllerTest.java
@@ -0,0 +1,42 @@
+package br.edu.ifpb.instagram.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldSignUpUser() throws Exception {
+        var request = new HashMap<String, Object>();
+        request.put("fullName", "Lucas");
+        request.put("username", "lucas_" + System.currentTimeMillis());
+        request.put("email", "lucas" + System.currentTimeMillis() + "@email.com");
+        request.put("password", "123456");
+
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+    }
+
+}
