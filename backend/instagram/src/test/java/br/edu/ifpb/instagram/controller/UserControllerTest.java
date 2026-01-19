3556a243f7c1daba82bae4935fbbfb37a62bbb0a
RogerioSouza-f <souzarogerio33freitas@gmail.com>
2026-01-19 19:25:04 -0300

    refactor:/ reajustes no UserControllerTest

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/UserControllerTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/UserControllerTest.java
@@ -7,29 +7,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.put;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test")
-@WebMvcTest(UserController.class)
-public class UserControllerTest {
+class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -37,17 +33,12 @@ public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+
     @MockitoBean
     private UserService userService;
 
     @Test
-     void shouldReturnUser() throws Exception {
-
-    }
-
-    @Test
     void shouldReturnListOfUsers() throws Exception {
-
         List<UserDto> users = List.of(
                 new UserDto(1L, "Lucas Cruz", "lucas", "lucas@email.com", null, null),
                 new UserDto(2L, "Maria Silva", "maria", "maria@email.com", null, null)
@@ -57,26 +48,18 @@ public class UserControllerTest {
 
         mockMvc.perform(get("/users"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(jsonPath("$.length()").value(2))
                 .andExpect(jsonPath("$[0].fullName").value("Lucas Cruz"))
                 .andExpect(jsonPath("$[1].username").value("maria"));
     }
 
     @Test
     void shouldReturnUserById() throws Exception {
-
-        UserDto user = new UserDto(
-                1L,
-                "Lucas Cruz",
-                "lucas",
-                "lucas@email.com",
-                null,
-                null
-        );
+        UserDto user = new UserDto(1L, "Lucas Cruz", "lucas", "lucas@email.com", null, null);
 
         when(userService.findById(1L)).thenReturn(user);
 
-        mockMvc.perform(get("{id}", 1L))
+        mockMvc.perform(get("/users/{id}", 1L))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1L))
                 .andExpect(jsonPath("$.username").value("lucas"));
@@ -84,7 +67,6 @@ public class UserControllerTest {
 
     @Test
     void shouldUpdateUser() throws Exception {
-
         UserDetailsRequest request = new UserDetailsRequest(
                 1L,
                 "Lucas Atualizado",
@@ -102,8 +84,7 @@ public class UserControllerTest {
                 null
         );
 
-        when(userService.updateUser(org.mockito.ArgumentMatchers.any()))
-                .thenReturn(updatedUser);
+        when(userService.updateUser(any())).thenReturn(updatedUser);
 
         mockMvc.perform(put("/users")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -114,12 +95,10 @@ public class UserControllerTest {
 
     @Test
     void shouldDeleteUser() throws Exception {
-
         doNothing().when(userService).deleteUser(1L);
 
-        mockMvc.perform(delete("/{id}", 1L))
+        mockMvc.perform(delete("/users/{id}", 1L))
                 .andExpect(status().isOk())
                 .andExpect(content().string("user was deleted!"));
     }
-
 }
