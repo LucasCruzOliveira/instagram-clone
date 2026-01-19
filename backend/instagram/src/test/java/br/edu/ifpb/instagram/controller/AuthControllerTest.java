d4c27001feb5fff5d565990f523ee924e42bff19
RogerioSouza-f <souzarogerio33freitas@gmail.com>
2026-01-19 19:05:14 -0300

    refactor:/ reajustes no AuthControllerTest

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/AuthControllerTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/AuthControllerTest.java
@@ -1,5 +1,8 @@
 package br.edu.ifpb.instagram.controller;
 
+import br.edu.ifpb.instagram.model.dto.UserDto;
+import br.edu.ifpb.instagram.service.UserService;
+import br.edu.ifpb.instagram.service.impl.AuthServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +10,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashMap;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test")
 class AuthControllerTest {
 
@@ -25,18 +31,64 @@ class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+
+    @MockitoBean
+    private AuthServiceImpl authService;
+
+    @MockitoBean
+    private UserService userService;
+
+    // ================= SIGNUP =================
+
     @Test
-    void shouldSignUpUser() throws Exception {
-        var request = new HashMap<String, Object>();
-        request.put("fullName", "Lucas");
-        request.put("username", "lucas_" + System.currentTimeMillis());
-        request.put("email", "lucas" + System.currentTimeMillis() + "@email.com");
-        request.put("password", "123456");
+    void shouldSignUpUserSuccessfully() throws Exception {
+
+        when(userService.createUser(any()))
+                .thenReturn(mockUserDto());
 
         mockMvc.perform(post("/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
+                        .content(objectMapper.writeValueAsString(validRequest())))
                 .andExpect(status().isCreated());
     }
 
+    // ================= SIGNIN =================
+
+    @Test
+    void shouldSignInSuccessfully() throws Exception {
+
+        when(authService.authenticate(any()))
+                .thenReturn("fake-jwt-token");
+
+        var request = new HashMap<String, Object>();
+        request.put("username", "admin");
+        request.put("password", "admin");
+
+        mockMvc.perform(post("/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+    }
+
+    // ================= AUX =================
+
+    private HashMap<String, Object> validRequest() {
+        var request = new HashMap<String, Object>();
+        request.put("fullName", "Lucas");
+        request.put("username", "lucas_123");
+        request.put("email", "lucas@email.com");
+        request.put("password", "123456");
+        return request;
+    }
+
+    private UserDto mockUserDto() {
+        return new UserDto(
+                1L,
+                "Lucas",
+                "lucas_123",
+                "lucas@email.com",
+                "123456",
+                null
+        );
+    }
 }
